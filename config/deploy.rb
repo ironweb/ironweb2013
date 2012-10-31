1dbd254998ea65864a75186cede593c8a6583b60
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2012-10-30 21:09:16 -0400

    Basic static deploy to s3 setup with s3-static-site gem

---
+++ config/deploy.rb
@@ -0,0 +1,7 @@
+require 's3-static-site'
+
+s3 = YAML::load( File.open( File.expand_path( '../s3.yml', __FILE__ ) ) )
+
+set :bucket, "ironweb.org"
+set :access_key_id, s3["access_key_id"]
+set :secret_access_key, s3["secret_access_key"]
